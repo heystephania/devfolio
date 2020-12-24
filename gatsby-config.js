e964251b9c696137f8eda9b2aa2fcfa0979ba122
heystephania <60100324+heystephania@users.noreply.github.com>
2020-12-23 19:18:44 -0500

    Update gatsby-config.js
    Changed url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://hard-fought-structu.000webhostapp.com/`,
     // Your Name
     name: 'Ryan Fitzgerald',
     // Main Site Title
